a48bc65f120da78f905f9f0c19904fa7984b2d3d
mohammed_alj <xqg1122@gmail.com>
2023-07-24 19:41:58 +0300

    Create the register page.

--- app.py
+++ app.py
@@ -2,10 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+logged_in = False
+
 
 @app.route("/")
 def index():
-    return render_template("index.html", is_logged_in=False)
+    return render_template("index.html", is_logged_in=logged_in)
 
 
-app.run(debug=True)
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html", is_logged_in=logged_in)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
