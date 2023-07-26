9de654219143af308d8a6d2f8d21a8eb865edefb
mohammed_alj <xqg1122@gmail.com>
2023-07-26 05:21:33 +0300

    Validate the password.

--- app.py
+++ app.py
@@ -18,6 +18,20 @@ def register():
         if len(username) < 5 or len(username) > 16:
             return render_template('error.html', error_code=403, message='Username length must be between 5 and 16 characters.')
 
+        password = request.form.get('password')
+        num_char_freq = {
+            'numbers': 0,
+            'characters': 0
+        }
+        for c in password:
+            if c.isascii():
+                num_char_freq["characters"] += 1
+            elif c.isdigit:
+                num_char_freq['numbers'] += 1
+
+        if num_char_freq['characters'] < 4 or num_char_freq['numbers'] < 2:
+            return render_template('error.html', error_code=403, message='Password must atleast contain 4 characters and 2 numbers.')
+
     return render_template("register.html", is_logged_in=logged_in)
 
 
